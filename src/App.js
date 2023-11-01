b0d3cc67db5697b646cde0c8c67e28f63d1f3c16
Hamzah <hamzahchhapra2004@gmail.com>
2023-11-01 21:41:23 +0530

    changed "todoList" to "todoArr"
    processing of data part 1
    saving input in todoArr for later processing

--- src/App.js
+++ src/App.js
@@ -4,25 +4,37 @@ import React, { useState } from "react";
 function App() {
   const handleSubmit = () => {
     if (inputValue === "Type something to update here") {
-      return console.error("Value change kar bhai");
+      return console.error("Value toh add kar bhai");
     }
-    setTodoList((preValue) => [...preValue, inputValue]);
+    // setTodoList((preValue) => [...preValue, inputValue]);
+    setTodoArr((preValue) => [...preValue, inputValue]);
+    document.getElementById("task-input").value = ""
+    /*todoList = todoArr.map((task) => 
+      <>
+        <h1>{task}</h1>
+        <hr />
+        <br />
+      </>
+    )*/
   };
-  const [todoList, setTodoList] = useState("");
+  const [todoArr, setTodoArr] = useState([]);
   const [inputValue, setInputValue] = useState("Type something to update here");
+  if (inputValue === "") {
+    setInputValue("Type something to update here")
+  }
   return (
     <div>
       <h1>Basic Setup</h1>
       <div>
-        <input onChange={(e) => setInputValue(e.target.value)} />
+        <input id="task-input" onChange={(e) => setInputValue(e.target.value)} />
         <h4 onClick={handleSubmit}>Button</h4>
-        <h4 onClick={() => console.log(todoList)}>Show</h4>
-        <h4 onClick={() => setTodoList("")}>Clear</h4>
+        <h4 onClick={() => console.log(todoArr)}>Show</h4>
+        <h4 onClick={() => setTodoArr("")}>Clear</h4>
       </div>
       <div>
         {inputValue}
         <hr />
-        {todoList}
+        {todoArr}
       </div>
     </div>
   );
